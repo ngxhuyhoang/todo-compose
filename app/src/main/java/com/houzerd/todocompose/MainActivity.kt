de9e878ff202c129ba854527f6f32da670617876
Huy Hoang Nguyen <ngxhuyhoang@gmail.com>
2021-08-22 01:11:34 +0700

    dfbdfb

--- app/src/main/java/com/houzerd/todocompose/MainActivity.kt
+++ app/src/main/java/com/houzerd/todocompose/MainActivity.kt
@@ -33,9 +33,6 @@ fun App() {
     val todos = mutableListOf("Go to school", "Go to work", "Go to home")
     val textState = remember { mutableStateOf(TextFieldValue()) }
 
-
-    
-
     Column {
         TopAppBar(title = { Text("Todo App Compose") })
         Row(verticalAlignment = Alignment.CenterVertically) {
