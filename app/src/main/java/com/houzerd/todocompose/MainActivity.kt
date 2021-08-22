70a32b958fae8ed7711d562cd7200e9ab49a3be6
Huy Hoang Nguyen <ngxhuyhoang@gmail.com>
2021-08-22 14:26:53 +0700

    update

--- app/src/main/java/com/houzerd/todocompose/MainActivity.kt
+++ app/src/main/java/com/houzerd/todocompose/MainActivity.kt
@@ -3,20 +3,17 @@ package com.houzerd.todocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
+import com.houzerd.todocompose.ui.composable.App
 import com.houzerd.todocompose.ui.theme.TodoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TodoComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -25,37 +22,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun App() {
-    val todos = mutableListOf("Go to school", "Go to work", "Go to home")
-    val textState = remember { mutableStateOf(TextFieldValue()) }
-
-    Column {
-        TopAppBar(title = { Text("Todo App Compose") })
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            TextField(
-                value = textState.value,
-                onValueChange = { textState.value = it },
-                placeholder = { Text(text = "Add new Todo") }
-            )
-            Button(onClick = { }, content = { Text("Add") })
-        }
-
-        Column {
-            todos.forEach {
-                todo -> Text(todo)
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodoComposeTheme {
-        App()
     }
 }
---
+++ app/src/main/java/com/houzerd/todocompose/ui/composable/App.kt
@@ -0,0 +1,39 @@
+package com.houzerd.todocompose.ui.composable
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun App() {
+    val taskInput = remember { mutableStateOf("") }
+
+    Column {
+        TopAppBar(
+            title = { Text("Todo Compose") }
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = taskInput.value,
+                onValueChange = { taskInput.value = it },
+                placeholder = { Text("New task here") },
+                label = { Text("New task here") }
+            )
+            Button(
+                onClick = { print("Pressed") },
+                content = { Text("Add new task") },
+            )
+        }
+    }
+}
